5187e0bcecddb9e1c3bc2f4909ea274adf506b1e
sjfhsjfh <sjfhsjfh@qq.com>
2025-03-09 13:48:51 +0800

    feat: allow rename

--- typst-wasm-macros/src/lib.rs
+++ typst-wasm-macros/src/lib.rs
@@ -1,18 +1,27 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote, ToTokens};
+use syn::{parse::Parser, Expr};
 use venial::*;
 
 fn construct_export(
     name: &proc_macro2::Ident,
     vis_marker: Option<VisMarker>,
     params: Vec<FnTypedParam>,
+    export_rename: &Option<Expr>,
 ) -> proc_macro2::TokenStream {
     fn param_idx(name: &proc_macro2::Ident) -> proc_macro2::Ident {
         format_ident!("__{}_idx", name)
     }
 
     let inner_name = format_ident!("__wasm_minimal_protocol_internal_function_{}", name);
-    let export_name = proc_macro2::Literal::string(&name.to_string());
+    // let export_name = proc_macro2::Literal::string(&name.to_string());
+    let export_name = export_rename
+        .as_ref()
+        .unwrap_or(&Expr::Lit(syn::ExprLit {
+            attrs: vec![],
+            lit: syn::Lit::Str(syn::LitStr::new(&name.to_string(), name.span())),
+        }))
+        .to_token_stream();
     let p_idx = params
         .iter()
         .map(|p| &p.name)
@@ -56,8 +65,29 @@ fn construct_export(
     .into()
 }
 
+struct MacroOptions {
+    pub export_rename: Option<Expr>,
+}
+
 #[proc_macro_attribute]
-pub fn wasm_export(_: TokenStream, item: TokenStream) -> TokenStream {
+pub fn wasm_export(args: TokenStream, item: TokenStream) -> TokenStream {
+    let args = syn::punctuated::Punctuated::<syn::MetaNameValue, syn::Token![,]>::parse_terminated
+        .parse(args)
+        .expect("invalid arguments");
+
+    let mut options = MacroOptions {
+        export_rename: None,
+    };
+    for arg in args {
+        let syn::MetaNameValue { path, value, .. } = arg;
+        match path.get_ident().expect("invalid argument") {
+            x if x == "export_rename" => {
+                options.export_rename = Some(value);
+            }
+            _ => panic!("invalid argument"),
+        }
+    }
+
     let mut item = proc_macro2::TokenStream::from(item);
     let decl = parse_item(item.clone()).expect("invalid declaration");
     let func = match decl.as_function() {
@@ -98,7 +128,12 @@ pub fn wasm_export(_: TokenStream, item: TokenStream) -> TokenStream {
     if let Some(error) = error {
         result.extend(error.to_compile_error());
     } else {
-        result.extend(construct_export(&name, vis_marker, p));
+        result.extend(construct_export(
+            &name,
+            vis_marker,
+            p,
+            &options.export_rename,
+        ));
     }
     result.into()
 }
